ad04d0cf2f5c774da5809f76156d5bb4fdebb7cb
Vignesh S <vigneshvirshow003@gmail.com>
2025-09-01 15:53:43 +0530

    Initial commit

---
+++ Assets.API/Dtos/SignupRequest.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Domain.Entities;
+
+namespace Assets.API.Dtos
+{
+    public class SignupRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+    }
+}
