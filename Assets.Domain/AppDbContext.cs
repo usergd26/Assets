5edd5c33789d24f47db34534da20222f506d063a
Vignesh S <vigneshvirshow003@gmail.com>
2025-09-01 16:56:11 +0530

    Migration files

--- Assets.Domain/AppDbContext.cs
+++ Assets.Domain/AppDbContext.cs
@@ -9,6 +9,8 @@ namespace Assets.Domain
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AssetType> AssetTypes { get; set; }
+        public DbSet<Asset> Assets { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -28,7 +30,26 @@ namespace Assets.Domain
                 entity.HasIndex(u => u.Email)
                        .IsUnique();
             });
-        }
 
+            // AssetType <-> Asset relationship
+            modelBuilder.Entity<AssetType>(entity =>
+            {
+                entity.Property(at => at.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.HasMany(at => at.Assets)
+                      .WithOne(a => a.AssetType)
+                      .HasForeignKey(a => a.AssetTypeId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Asset>(entity =>
+            {
+                entity.Property(a => a.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+            });
+        }
     }
 }
