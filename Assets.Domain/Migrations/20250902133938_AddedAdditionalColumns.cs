14c345339f3adc9b435de5a0be0f6aa1c0319ed2
Vignesh S <vigneshvirshow003@gmail.com>
2025-09-02 19:36:13 +0530

    Migration files

---
+++ Assets.Domain/Migrations/20250902133938_AddedAdditionalColumns.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assets.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAdditionalColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Assets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Assets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModelNo",
+                table: "Assets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Assets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "ModelNo",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Assets");
+        }
+    }
+}
